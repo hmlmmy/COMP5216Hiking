f9b5bb516c4a7a56e3929e68b7742c34023979bd
hmlmmy <feiyang.weng99@gmail.com>
2023-10-02 22:18:45 +1100

    event page initialize

---
+++ app/src/main/java/comp5216/sydney/edu/au/hiketogether/EventPageActivity.java
@@ -0,0 +1,14 @@
+package comp5216.sydney.edu.au.hiketogether;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_page);
+    }
+}
