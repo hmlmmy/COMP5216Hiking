3f6bda0f2edfd8b4879e9f34989ee357c731152e
hmlmmy <feiyang.weng99@gmail.com>
2023-10-06 19:28:36 +1100

    feiyang added pages jump function

--- app/src/main/java/comp5216/sydney/edu/au/hiketogether/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/hiketogether/MainActivity.java
@@ -2,7 +2,10 @@ package comp5216.sydney.edu.au.hiketogether;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonHome = findViewById(R.id.buttonHome);
+        Button buttonEvent = findViewById(R.id.buttonEvent);
+        Button buttonCreateEvent = findViewById(R.id.buttonCreateEvent);
+        Button buttonProfile = findViewById(R.id.buttonProfile);
+
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
+
+        buttonEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EventPageActivity.class));
+            }
+        });
+
+        buttonCreateEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreateEventActivity.class));
+            }
+        });
+
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            }
+        });
     }
 }
