7cd1a46004ac7384e38cf8596f9927d6d6b24db8
NuojinZhang <hunter19991113@gmail.com>
2023-10-13 20:30:19 +1100

    edit_profile page added

---
+++ app/src/main/java/comp5216/sydney/edu/au/hiketogether/EditProfile.java
@@ -0,0 +1,13 @@
+package comp5216.sydney.edu.au.hiketogether;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
