46f642575c94378eae9bbf173a832c6d233d08a5
hmlmmy <feiyang.weng99@gmail.com>
2023-10-21 00:08:01 +1100

    fix link bug with firebase of event detail page

--- app/src/main/java/comp5216/sydney/edu/au/hiketogether/EventDetailActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/hiketogether/EventDetailActivity.java
@@ -149,10 +149,8 @@ public class EventDetailActivity extends AppCompatActivity {
         joinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 在这里执行 "Join" 操作的代码
-
                 // 创建对用户文档的引用，使用 whereEqualTo 查询邮箱地址
-                db.collection("userProfiles")
+                db.collection("User Profile")
                         .whereEqualTo("email", userEmail) // userEmail 是当前用户的邮箱地址
                         .get()
                         .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
@@ -162,45 +160,42 @@ public class EventDetailActivity extends AppCompatActivity {
                                     // 获取匹配邮箱的用户文档
                                     DocumentSnapshot userDocument = queryDocumentSnapshots.getDocuments().get(0);
 
-                                    // 在此处执行 "Join" 操作，如将事件添加到用户的 bookedEvents 数组中
-                                    // 假设你已经获取了当前事件的 ID（eventId）。
+                                    // 获取用户的 joinedEvents 数组
+                                    List<String> joinedEvents = (List<String>) userDocument.get("joinedEvents");
 
-                                    // 获取用户的 bookedEvents 数组
-                                    List<String> bookedEvents = (List<String>) userDocument.get("bookedEvents");
-
-                                    if (bookedEvents == null) {
-                                        bookedEvents = new ArrayList<>();
+                                    if (joinedEvents == null) {
+                                        joinedEvents = new ArrayList<>();
                                     }
 
                                     // 检查用户是否已经预定了这个事件
-                                    if (!bookedEvents.contains(eventId)) {
-                                        // 如果用户没有预定这个事件，将事件 ID 添加到 bookedEvents 数组
-                                        bookedEvents.add(eventId);
+                                    if (!joinedEvents.contains(eventId)) {
+                                        // 如果用户没有预定这个事件，将事件 ID 添加到 joinedEvents 数组
+                                        joinedEvents.add(eventId);
 
                                         // 更新用户文档
-                                        userDocument.getReference().update("bookedEvents", bookedEvents)
+                                        userDocument.getReference().update("joinedEvents", joinedEvents)
                                                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                                                     @Override
                                                     public void onSuccess(Void aVoid) {
                                                         // 更新成功，用户已成功加入事件
                                                         // 可以在这里执行相应的操作，例如显示消息
-                                                        Toast.makeText(EventDetailActivity.this, "加入成功！", Toast.LENGTH_SHORT).show();
+                                                        Toast.makeText(EventDetailActivity.this, "Join successfully!", Toast.LENGTH_SHORT).show();
                                                     }
                                                 })
                                                 .addOnFailureListener(new OnFailureListener() {
                                                     @Override
                                                     public void onFailure(@NonNull Exception e) {
                                                         // 更新失败，处理错误
-                                                        Toast.makeText(EventDetailActivity.this, "加入失败：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                        Toast.makeText(EventDetailActivity.this, "Join failure!" + e.getMessage(), Toast.LENGTH_SHORT).show();
                                                     }
                                                 });
                                     } else {
-                                        // 用户已经预定了这个事件，你可以在这里显示相应的消息
-                                        Toast.makeText(EventDetailActivity.this, "您已经预定了这个事件！", Toast.LENGTH_SHORT).show();
+                                        // 用户已经预定了这个事件
+                                        Toast.makeText(EventDetailActivity.this, "You have joined this event", Toast.LENGTH_SHORT).show();
                                     }
                                 } else {
                                     // 没有匹配邮箱的用户文档，处理错误
-                                    Toast.makeText(EventDetailActivity.this, "找不到用户文档", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(EventDetailActivity.this, "Can not find user documentation", Toast.LENGTH_SHORT).show();
                                 }
                             }
                         })
@@ -208,7 +203,7 @@ public class EventDetailActivity extends AppCompatActivity {
                             @Override
                             public void onFailure(@NonNull Exception e) {
                                 // 查询失败，处理错误
-                                Toast.makeText(EventDetailActivity.this, "查询用户文档失败：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                Toast.makeText(EventDetailActivity.this, "Fail to query user documents: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                             }
                         });
             }
@@ -218,10 +213,8 @@ public class EventDetailActivity extends AppCompatActivity {
         quitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 在这里执行 "Quit" 操作的代码
-
                 // 创建对用户文档的引用，使用 whereEqualTo 查询邮箱地址
-                db.collection("userProfiles")
+                db.collection("User Profile")
                         .whereEqualTo("email", userEmail) // userEmail 是当前用户的邮箱地址
                         .get()
                         .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
@@ -231,43 +224,40 @@ public class EventDetailActivity extends AppCompatActivity {
                                     // 获取匹配邮箱的用户文档
                                     DocumentSnapshot userDocument = queryDocumentSnapshots.getDocuments().get(0);
 
-                                    // 在此处执行 "Quit" 操作，如从用户的 bookedEvents 数组中移除事件 ID
-                                    // 假设你已经获取了当前事件的 ID（eventId）。
+                                    // 获取用户的 joinedEvents 数组
+                                    List<String> joinedEvents = (List<String>) userDocument.get("joinedEvents");
 
-                                    // 获取用户的 bookedEvents 数组
-                                    List<String> bookedEvents = (List<String>) userDocument.get("bookedEvents");
-
-                                    if (bookedEvents != null) {
+                                    if (joinedEvents != null) {
                                         // 检查用户是否已经预定了这个事件
-                                        if (bookedEvents.contains(eventId)) {
-                                            // 如果用户已经预定这个事件，从 bookedEvents 数组中移除事件 ID
-                                            bookedEvents.remove(eventId);
+                                        if (joinedEvents.contains(eventId)) {
+                                            // 如果用户已经预定这个事件，从 joinedEvents 数组中移除事件 ID
+                                            joinedEvents.remove(eventId);
 
                                             // 更新用户文档
-                                            userDocument.getReference().update("bookedEvents", bookedEvents)
+                                            userDocument.getReference().update("joinedEvents", joinedEvents)
                                                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                                                         @Override
                                                         public void onSuccess(Void aVoid) {
                                                             // 更新成功，用户已成功退出事件
                                                             // 可以在这里执行相应的操作，例如显示消息
-                                                            Toast.makeText(EventDetailActivity.this, "退出成功！", Toast.LENGTH_SHORT).show();
+                                                            Toast.makeText(EventDetailActivity.this, "Quit successfully!", Toast.LENGTH_SHORT).show();
                                                         }
                                                     })
                                                     .addOnFailureListener(new OnFailureListener() {
                                                         @Override
                                                         public void onFailure(@NonNull Exception e) {
                                                             // 更新失败，处理错误
-                                                            Toast.makeText(EventDetailActivity.this, "退出失败：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                            Toast.makeText(EventDetailActivity.this, "Quit failure: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                                                         }
                                                     });
                                         } else {
                                             // 用户没有预定这个事件，你可以在这里显示相应的消息
-                                            Toast.makeText(EventDetailActivity.this, "您未预定这个事件！", Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(EventDetailActivity.this, "You have not joined this event", Toast.LENGTH_SHORT).show();
                                         }
                                     }
                                 } else {
                                     // 没有匹配邮箱的用户文档，处理错误
-                                    Toast.makeText(EventDetailActivity.this, "找不到用户文档", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(EventDetailActivity.this, "Can not find user documentation", Toast.LENGTH_SHORT).show();
                                 }
                             }
                         })
@@ -275,7 +265,7 @@ public class EventDetailActivity extends AppCompatActivity {
                             @Override
                             public void onFailure(@NonNull Exception e) {
                                 // 查询失败，处理错误
-                                Toast.makeText(EventDetailActivity.this, "查询用户文档失败：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                Toast.makeText(EventDetailActivity.this, "Fail to query user documents: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                             }
                         });
             }
